8bc773fcc4a99976026f594bab15e34f447a2ee8
Bayu <bayu.aditya@sci.ui.ac.id>
2019-07-17 17:13:29 +0700

    Add docstring in class extract parameter

--- Tight_Binding/extract_parameter.py
+++ Tight_Binding/extract_parameter.py
@@ -3,6 +3,12 @@ import numpy as np
 import pandas as pd
 
 class extract_parameter():
+    """
+    Ekstrak Parameter dari file output Wannier90 *_hr.dat . Data parameter ditampilkan dalam bentuk DataFrame.
+
+    Args:
+        hr_files (str): nama dan lokasi file hr
+    """
     def __init__(self, hr_files):
         num_init_row = self._check_initial_row(hr_files)
         self.data = pd.read_fwf(
@@ -11,10 +17,27 @@ class extract_parameter():
             skiprows=num_init_row
             )
 
+
     def get_data(self):
+        """
+        Mengambil parameter berbentuk DataFrame
+
+        Returns:
+            pandas.DataFrame: Parameter Tight Binding
+        """
         return self.data
-    
+
+
     def _check_initial_row(self, hr_files):
+        """
+        Menentukan letak baris awal pembacaan parameter
+
+        Args:
+            hr_files (str): nama dan lokasi file hr
+        
+        Returns:
+            int: nomor baris awal pembacaan parameter
+        """
         with open(hr_files) as f:
             data = f.readlines()
         for num_row, row in enumerate(data):
